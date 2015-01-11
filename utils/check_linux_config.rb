4421429199d32e1cd4202ad97c38bc36701b00e8
Jun Wu <quark@lihdd.net>
2015-01-11 19:00:26 +0800

    Note Debian has disabled fanotify kernel config
    It means --fopen-filter cannot be used in Debian.
    Umm.... I am tempted to build a custom kernel package
    for Debian then.

--- utils/check_linux_config.rb
+++ utils/check_linux_config.rb
@@ -55,11 +55,11 @@ check_config '    cpuacct',   'CONFIG_CGROUP_CPUACCT'
 
 # Since 2.6.39-bpo60-2 for Squeeze the memory cgroup support is built in
 if check_kernel [0]..[2, 6, 39]
-  check_config '    memory',    'CONFIG_CGROUP_MEM_RES_CTLR'
+  check_config '    memory',  'CONFIG_CGROUP_MEM_RES_CTLR'
 end
 check_config '    freezer',   'CONFIG_CGROUP_FREEZER'
 
 check_config 'fanotify',      'CONFIG_FANOTIFY'
-check_config '    check', 'CONFIG_FANOTIFY_ACCESS_PERMISSIONS'
+check_config '    check',     'CONFIG_FANOTIFY_ACCESS_PERMISSIONS'
 
 exit(@no_problem ? 0 : 1)
